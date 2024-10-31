70396ad59690def44470b4f40a75735ffb5a51fd
phancongquoc <phanquoc870@gmail.com>
2024-10-31 20:33:15 +0700

    Initial commit

---
+++ app/src/main/java/com/example/bt_content_provider/Message.java
@@ -0,0 +1,14 @@
+package com.example.bt_content_provider;
+
+public class Message {
+    private String address;
+    private String body;
+
+    public Message(String address, String body) {
+        this.address = address;
+        this.body = body;
+    }
+
+    public String getAddress() { return address; }
+    public String getBody() { return body; }
+}
